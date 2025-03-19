ae558e8c2273785f172b2b80d8f4bbb589d50505
kritique <128252454+kritiquekapital@users.noreply.github.com>
2025-03-19 18:56:47 -0400

    Update photoButton_0.9.js

--- js/photoButton_0.9.js
+++ js/photoButton_0.9.js
@@ -1,6 +1,37 @@
 const imageList = [
   "roll_03_01.jpg",
-  // Add other image names
+  "roll_03_(02).jpg",
+  "roll_03_(03).jpg",
+  "roll_03_(04).jpg",
+  "roll_03_(05).jpg",
+  "roll_03_(06).jpg",
+  "roll_03_(07).jpg",
+  "roll_03_(08).jpg",
+  "roll_03_(09).jpg",
+  "roll_03_(10).jpg",
+  "roll_03_(11).jpg",
+  "roll_03_(12).jpg",
+  "roll_03_(13).jpg",
+  "roll_03_(14).jpg",
+  "roll_03_(15).jpg",
+  "roll_03_(16).jpg",
+  "roll_03_(17).jpg",
+  "roll_03_(18).jpg",
+  "roll_03_(19).jpg",
+  "roll_03_(20).jpg",
+  "roll_03_(21).jpg",
+  "roll_03_(22).jpg",
+  "roll_03_(23).jpg",
+  "roll_03_(24).jpg",
+  "roll_03_(25).jpg",
+  "roll_03_(26).jpg",
+  "roll_03_(27).jpg",
+  "roll_03_(28).jpg",
+  "roll_03_(29).jpg",
+  "roll_03_(30).jpg",
+  "roll_03_(31).jpg",
+  "roll_03_(32).jpg",
+  "roll_03_(33).jpg"
 ];
 
 let currentIndex = 0;
