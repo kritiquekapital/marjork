c45d8101433d8777b4390767589cb8048c3b1df0
kritique <128252454+kritiquekapital@users.noreply.github.com>
2025-03-28 01:01:53 -0400

    Create draggable.js

---
+++ draggable.js
@@ -0,0 +1,35 @@
+// draggable.js
+export class Draggable {
+  constructor(element, handleSelector = '.drag-handle') {
+    this.element = element;
+    this.handle = element.querySelector(handleSelector);
+    this.isDragging = false;
+    this.offset = { x: 0, y: 0 };
+
+    this.init();
+  }
+
+  init() {
+    this.handle.addEventListener('mousedown', this.startDrag.bind(this));
+    document.addEventListener('mousemove', this.drag.bind(this));
+    document.addEventListener('mouseup', this.stopDrag.bind(this));
+  }
+
+  startDrag(e) {
+    this.isDragging = true;
+    this.offset = {
+      x: e.clientX - this.element.offsetLeft,
+      y: e.clientY - this.element.offsetTop
+    };
+  }
+
+  drag(e) {
+    if (!this.isDragging) return;
+    this.element.style.left = `${e.clientX - this.offset.x}px`;
+    this.element.style.top = `${e.clientY - this.offset.y}px`;
+  }
+
+  stopDrag() {
+    this.isDragging = false;
+  }
+}
