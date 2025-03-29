fe673bb717968d3aadab092553de8f6de2caed03
kritique <128252454+kritiquekapital@users.noreply.github.com>
2025-03-29 14:59:48 -0400

    Update musicPlayer_1.0.js

--- js/musicPlayer_1.0.js
+++ js/musicPlayer_1.0.js
@@ -15,11 +15,30 @@ new Draggable(musicPlayer, '.ipod-screen');
 
 // Video Links
 const liveLinks2 = [
-  "https://www.youtube.com/embed/x3xYXGMRRYk?autoplay=1",
-  "https://www.youtube.com/embed/L1Snj1Pt-Hs?autoplay=1",
-  "https://www.youtube.com/embed/_KztNIg4cvE?autoplay=1",
-  "https://www.youtube.com/embed/_6rUeOCm7S0?autoplay=1",
-  "https://www.youtube.com/embed/__xsCTe9dTQ?autoplay=1"
+  "https://www.youtube.com/embed/x3xYXGMRRYk?autoplay=1",  //Candy
+  "https://www.youtube.com/embed/L1Snj1Pt-Hs?autoplay=1",  //Плачу на техно
+  "https://www.youtube.com/embed/_KztNIg4cvE?autoplay=1",  //Gypsy Woman
+  "https://www.youtube.com/embed/_6rUeOCm7S0?autoplay=1",  //Volga
+  "https://www.youtube.com/embed/__xsCTe9dTQ?autoplay=1",  //но останься
+  "https://www.youtube.com/embed/B6Y-WsgpzlQ?autoplay=1",  //False Sympathy
+  "https://www.youtube.com/embed/6riDJMI-Y8U?autoplay=1",  //Lost
+  "https://www.youtube.com/embed/PPoH0Gn50Nc?autoplay=1",  //Renegades
+  "https://www.youtube.com/embed/taCRBFkUqdM?autoplay=1",  //Let Me
+  "https://www.youtube.com/embed/y1TNuHPSBXI?autoplay=1",  //Loosen Up
+  "https://www.youtube.com/embed/LSIOcCcEVaE?autoplay=1",  //SnSORRY
+  "https://www.youtube.com/embed/BpqOWO6ctsg?autoplay=1",  //Sunshine
+  "https://www.youtube.com/embed/iYTz6lr8JY8?autoplay=1",  //Fam
+  "https://www.youtube.com/embed/7xxgRUyzgs0?autoplay=1",  //Cult
+  "https://www.youtube.com/embed/G4CKmzBf5Cs?autoplay=1",  //Mass
+  "https://www.youtube.com/embed/-rZWdolJfgk?autoplay=1",  //Potage
+  "https://www.youtube.com/embed/FEkOYs6aWIg?autoplay=1",  //Absolutely
+  "https://www.youtube.com/embed/I067BonnW48?autoplay=1",  //María
+  "https://www.youtube.com/embed/V7IUtUsfARA?autoplay=1",  //Like Kant
+  "https://www.youtube.com/embed/x4ygVwbOyJU?autoplay=1",  //Doomer
+  "https://www.youtube.com/embed/3NrZCJh2Hgk?autoplay=1",  //Seaside
+  "https://www.youtube.com/embed/cYpQ36acEUU?autoplay=1",  //miss u
+  "https://www.youtube.com/embed/wfj26-cQkx8?autoplay=1",  //forfeit
+  "https://www.youtube.com/embed/SMIQbo-61P4?autoplay=1",  //saftey
 ];
 
 let isFirstOpen = true;
