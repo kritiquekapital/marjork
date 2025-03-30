019eca18f117616b9ee19b2f760387dfc0f2b247
kritique <128252454+kritiquekapital@users.noreply.github.com>
2025-03-30 11:47:26 -0400

    Update vinylButton_0.9.js

--- js/vinylButton_0.9.js
+++ js/vinylButton_0.9.js
@@ -1,20 +1,3 @@
-const vinylLink = document.querySelector(".vinyl-link");
-const liveLinks2 = [
-  "https://www.youtube.com/watch?v=6riDJMI-Y8U",
-  // Add other vinyl links
-];
-
-let currentLinkIndex2 = 0;
-
-if (vinylLink) {
-  vinylLink.addEventListener("click", (event) => {
-    event.preventDefault();
-    const newTab = window.open(liveLinks2[currentLinkIndex2], "_blank");
-    if (newTab) newTab.blur();
-    currentLinkIndex2 = (currentLinkIndex2 + 1) % liveLinks2.length;
-  });
-}
-
 const vinylButton = document.querySelector(".vinyl");
 const arm = document.querySelector(".vinyl .arm");
 
