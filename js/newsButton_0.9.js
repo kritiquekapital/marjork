0fa53171c488564e8ac6b65fcffb01e2d23c1baa
kritique <128252454+kritiquekapital@users.noreply.github.com>
2025-04-03 01:53:11 -0400

    Update newsButton_0.9.js

--- js/newsButton_0.9.js
+++ js/newsButton_0.9.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const newsLinks = [
     "https://open.substack.com/pub/dropsitenews/p/gaza-children-israel-sniper-shoot-jazeera?r=jonc4&utm_medium=ios",
     "https://www.documentcloud.org/documents/25592020-letter-from-a-palestinian-political-prisoner-in-louisiana-march-18-2025/",
-    "https://www.salon.com/2001/10/16/susans/",
     "https://www.diagonalthoughts.com/?p=1728",
     "https://archive.org/details/palisriciailleursalighter",
+    "https://www.salon.com/2001/10/16/susans/",
     "https://mirror.xyz/sartoshi.eth/QukjtL1076-1SEoNJuqyc-x4Ut2v8_TocKkszo-S_nU"
   ];
 
