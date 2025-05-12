79378ee68c3d09f427a85d291dcdff867ef465dc
kritique <128252454+kritiquekapital@users.noreply.github.com>
2025-05-12 01:58:46 -0400

    Update submit.js

--- api/minesweeper/submit.js
+++ api/minesweeper/submit.js
@@ -20,6 +20,5 @@ export default async function handler(req, res) {
     .insert([{ username, time, difficulty }]);
 
   if (error) return res.status(500).json({ error: error.message });
-
   return res.status(200).json({ success: true, data });
 }
