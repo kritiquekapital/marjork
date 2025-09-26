088bcb650ab64807469225e4942ee86bc2107172
kritique <128252454+kritiquekapital@users.noreply.github.com>
2025-09-25 23:09:44 -0400

    Update lofi.js

--- js/lofi.js
+++ js/lofi.js
@@ -1,8 +1,8 @@
 (function () {
   const THEME_CLASS = "theme-lofi";
   const lofiStreams = [
-    "https://www.youtube.com/embed/jfKfPfyJRdk?autoplay=1&controls=0&loop=1&mute=1",
-    "https://www.youtube.com/embed/Zq9-4INDsvY?autoplay=1&controls=0&loop=1&mute=1"
+    "https://www.youtube.com/embed/jfKfPfyJRdk?autoplay=1&controls=0&loop=1&mute=1&vq=hd1080",
+    "https://www.youtube.com/embed/Zq9-4INDsvY?autoplay=1&controls=0&loop=1&mute=1&vq=hd1080"
   ];
   let currentIndex = 0;
 
